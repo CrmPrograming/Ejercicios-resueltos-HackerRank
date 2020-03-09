016c72e2a62e7d967d0e5c160c6cd458a32a2282
César Ravelo Martínez <crmprograming@gmail.com>
2020-03-09 10:44:00 +0000

    Creada la solución de Java Static Initializer Block y mejorada la de Comparator

--- Medium/src/dataStructures/javaComparator/Solution.java
+++ Medium/src/dataStructures/javaComparator/Solution.java
@@ -6,13 +6,7 @@ class Checker implements Comparator<Player> {
 
 	@Override
 	public int compare(Player p1, Player p2) {
-		if (p1.score < p2.score)
-			return 1;
-		
-		if (p1.score > p2.score)
-			return -1;
-		
-		return p1.name.compareTo(p2.name);
+		return (p1.score == p2.score)? p1.name.compareTo(p2.name) : p2.score - p1.score;
 	}
 
 }
