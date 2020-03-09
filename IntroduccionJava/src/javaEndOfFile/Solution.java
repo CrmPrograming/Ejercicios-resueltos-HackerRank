f4f24e275330c1cb24b65a73555e83fe4f2662ec
César Ravelo Martínez <crmprograming@gmail.com>
2020-03-09 10:25:45 +0000

    Creada la solución al ejercicio Java End of File

---
+++ IntroduccionJava/src/javaEndOfFile/Solution.java
@@ -0,0 +1,16 @@
+package javaEndOfFile;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {        
+    	Scanner in = new Scanner(System.in);
+    	int n = 0;
+    	
+    	do {
+    		System.out.println(++n + " " + in.nextLine());
+    	} while(in.hasNext());
+    	
+    }
+}
